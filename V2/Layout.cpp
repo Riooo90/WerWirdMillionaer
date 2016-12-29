a17e1b5409d0cf0a04a4fe51bb4699812a377905
Riooo90 <riooo@nurfuerspam.de>
2016-12-29 19:29:28 +0100

    Fortschritt angepasst

--- V2/Layout.cpp
+++ V2/Layout.cpp
@@ -1,6 +1,6 @@
 /*grundlegende Variablen für Layout definieren:  */
 #include "stdafx.h"
-int startShowGeldliste = 55;
+int startShowGeldliste = 95;
 int aktiveStufe = 0;
 int i;
 char* GeldStufen[15] = { "50 EURO", "100 EURO", "200 EURO", "300 EURO", "500 EURO", "1000 EURO", "2000 EURO", "4000 EURO", "8000 EURO", "16000 EURO", "32000 EURO", "64000 EURO", "125000 EURO", "500000 EURO", "1 MILLION" };
@@ -9,13 +9,13 @@ char* GeldStufen[15] = { "50 EURO", "100 EURO", "200 EURO", "300 EURO", "500 EUR
 //Geldliste zeichnen:
 void zeichneFortschrittsanzeige(int aktiveStufe) {
 	for (int i = 0; i < 15; i++) {
-		text(startShowGeldliste + i * 2 * 30, GeldStufen[i]);
+		text(startShowGeldliste + i * 2 * 50, GeldStufen[i]);
 		if (i == aktiveStufe) {
-			farbe(startShowGeldliste - 1 + i * 2 * 30, GREEN);
-			farbe(startShowGeldliste + 1 + i * 2 * 30, GREEN);
-			farbe(startShowGeldliste + i * 2 * 30, GREEN);
+			farbe(startShowGeldliste - 1 + i * 2 * 50, GREEN);
+			farbe(startShowGeldliste + 1 + i * 2 * 50, GREEN);
+			farbe(startShowGeldliste + i * 2 * 50, GREEN);
 		}
-	}	senkrechtBeides(20, 0, 30, BLUE, "s");
+	}	senkrechtBeides(90, 0, 30, BLUE, "s");
 }
 
 //Einlog Button
