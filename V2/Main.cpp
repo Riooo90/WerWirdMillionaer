cd4932950ff59b4da4c1262dfc9297e5728dbf06
Riooo90 <riooo@nurfuerspam.de>
2016-12-29 18:23:41 +0100

    Projekt hinzugefuegt
    test

---
+++ V2/Main.cpp
@@ -0,0 +1,32 @@
+// V2.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+
+	/*grundlegende Variablen für Layout definieren: */
+int startShowGeldliste = 55;
+int aktiveStufe = 0;
+char* GeldStufen[15] = { "50 EURO", "100 EURO", "200 EURO", "300 EURO", "500 EURO", "1000 EURO", "2000 EURO", "4000 EURO", "8000 EURO", "16000 EURO", "32000 EURO", "64000 EURO", "125000 EURO", "500000 EURO", "1 MILLION" };
+int a = 30, b = 30;
+
+//Geldliste zeichnen:
+void zeichneFortschrittsanzeige(int aktiveStufe) {
+	for (int i = 0; i < 15; i++) {
+		text(startShowGeldliste + i * 2 * a, GeldStufen[i]);
+		if (i == aktiveStufe) {
+			farbe(startShowGeldliste-1 + i * 2 * a, GREEN);
+			farbe(startShowGeldliste+1 + i * 2 * a, GREEN);
+			farbe(startShowGeldliste + i * 2 * a, GREEN);
+		}
+	}	senkrechtBeides(20, 0, 30, BLUE, "s");
+}
+
+void main()
+{
+	loeschen();
+	groesse(a, b);
+	formen("s");
+	zeichneFortschrittsanzeige(0);
+	getchar();
+	zeichneFortschrittsanzeige(3);
+	getchar();
+}
---
+++ V2/funktionenEigen.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include "jclient.h"
+
+//Blatt5:
+
+void waagrechtFarbe(int s, int z, int n, int f) {
+	for (int i = 0; i < n; i++) {
+		farbe2(s + i, z, f);
+	}
+}
+void waagrechtForm(int s, int z, int n, char* f) {
+	for (int i = 0; i < n; i++) {
+		form2(s + i, z, f);
+	}
+}
+void waagrechtBeides(int s, int z, int n, int farbe, char* form) {
+	for (int i = 0; i < n; i++) {
+		farbe2(s + i, z, farbe);
+		form2(s + i, z, form);
+	}
+}
+void senkrechtFarbe(int s, int z, int n, int f) {
+	for (int i = 0; i < n; i++) {
+		farbe2(s, z + i, f);
+	}
+}
+void senkrechtForm(int s, int z, int n, char* f) {
+	for (int i = 0; i < n; i++) {
+		form2(s, z + i, f);
+	}
+}
+void senkrechtBeides(int s, int z, int n, int farbe, char* form) {
+	for (int i = 0; i < n; i++) {
+		farbe2(s, z + i, farbe);
+		form2(s, z + i, form);
+	}
+}
